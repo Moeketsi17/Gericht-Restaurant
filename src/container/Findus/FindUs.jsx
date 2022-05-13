e5cb08617daffa2e0ca7fdc8c697c3a263dc4a79
moeketsi <raymondkxng17@gmail.com>
2022-05-13 13:10:23 +0200

    commit

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import {SubHeading} from '../../components'
-import { images, data } from '../../constants';
+import { images } from '../../constants';
 
 const FindUs = () => (
-  <div cl>
-    
+  <div className='app__bg app__wrapper section__padding ' id='contact' >
+    <div className="app__wrapper_info">
+      <SubHeading title='Contact' />
+      <h1 className="headtext__cormorant" style={{marginBottom: '3rem'}} >Find Us</h1>
+
+      <div className="app__wrapper-content">
+        <p className="p__opensans">376 Mandela drive, Pretoria</p>
+        <p className="p__cormorant" style={{color: 'dcca87', margin: '2rem 0'}} >Open Hours</p>
+        <p className="p__opensans">Mon - Fri 09:00 am - 09:00 pm</p>
+        <p className="p__opensans">Sat - Sun 09:00 am - 00:00am</p>
+      </div>
+      <button className="custom__button" style={{marginTop: '2rem'}} >Visit Us</button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="find us" />
+    </div>
   </div>
 );
 
